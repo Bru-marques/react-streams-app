397999fd1771bb935c49cd35dee8c460dfe91eaa
Bruna Marques <brmarques1108@gmail.com>
2021-08-16 12:18:54 +0100

    working on routes

--- streams2/client/src/components/App.js
+++ streams2/client/src/components/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-import StreamList from "./streams/StreamList";
-import StreamCreate from "./streams/StreamCreate";
-import StreamEdit from "./streams/StreamEdit";
-import StreamDelete from "./streams/StreamDelete";
-import Header from "./Header";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
@@ -17,6 +17,7 @@ const App = () => {
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
